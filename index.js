b5b0b614d4301dcd4a316df3daf27f03f25fafdb
Waheed Uddin Ahmed <dmc4719@gmail.com>
2022-07-29 01:53:54 +0600

    added http server

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const port = process.env.port || 5000
+const http = require("http");
+const cors = require("cors");
 const app = express()
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.get('/', (req, res) => {
     return res.send('Welcome to api!')
 })
 
-app.listen(port, () => {
+app.get('/api/*', (req, res) => {
+    return res.send('Welcome to all api routes!')
+})
+const server = http.createServer(app);
+server.listen(port, () => {
     console.log(`app is running on port ${port}`)
 })
