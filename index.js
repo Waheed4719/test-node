47c208576473a4c6244c1972e4be2ec6707fb34c
Waheed Uddin Ahmed <dmc4719@gmail.com>
2022-07-29 00:42:02 +0600

    added an index route in index file

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = process.env.port || 5000
 const app = express()
 
 
+app.get('/', (req, res) => {
+    return res.send('Welcome to api!')
+})
+
 app.listen(port, () => {
     console.log(`app is running on port ${port}`)
 })
